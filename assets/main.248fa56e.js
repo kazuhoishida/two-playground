90db5f3afafe9106f4ba113812d7687278e020f6
Kazuho Ishida <kazuhoishida@Kazuhos-MacBook-Pro.local>
2022-04-27 20:10:27 +0900

    deploy

---
+++ assets/main.248fa56e.js
@@ -0,0 +1,1 @@
+import{j as e,N as t,c as r}from"./index.fdee6f95.js";function o(){return e("div",{className:"App",children:e(t,{})})}r.createRoot(document.getElementById("root")).render(e(React.StrictMode,{children:e(o,{})}));
