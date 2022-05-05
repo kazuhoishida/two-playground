6fe55d39fc740073172a28803d6708896219f414
Kazuho Ishida <k10700110@gmail.com>
2022-05-06 03:39:15 +0900

    build typefinder

--- src/dist/assets/main.99a925e5.js
+++
@@ -1,1 +0,0 @@
-import{j as e,c as r}from"./index.fafe8bdb.js";import{N as t}from"./NolookSidebarLeft.f77a508c.js";function o(){return e("div",{className:"App",children:e(t,{})})}r.createRoot(document.getElementById("root")).render(e(React.StrictMode,{children:e(o,{})}));
