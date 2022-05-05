9d2ca0148a8d5ce53834c5dac9c0222b9e5cade6
Kazuho Ishida <k10700110@gmail.com>
2022-05-06 03:46:05 +0900

    mod import images

--- src/components/card/cardImages.js
+++ src/components/card/cardImages.js
@@ -1,11 +1,11 @@
-export const cardImages = [
-  { src: "../../image/card/card01.png" },
-  { src: "../../image/card/card02.png" },
-  { src: "../../image/card/card03.png" },
-  { src: "../../image/card/card04.png" },
-  { src: "../../image/card/card05.png" },
-  { src: "../../image/card/card06.png" },
-  { src: "../../image/card/card07.png" },
-  { src: "../../image/card/card08.png" },
-  { src: "../../image/card/card09.png" },
-]
+import cardImg01 from "../../image/card/card01.png"
+import cardImg02 from "../../image/card/card02.png"
+import cardImg03 from "../../image/card/card03.png"
+import cardImg04 from "../../image/card/card04.png"
+import cardImg05 from "../../image/card/card05.png"
+import cardImg06 from "../../image/card/card06.png"
+import cardImg07 from "../../image/card/card07.png"
+import cardImg08 from "../../image/card/card08.png"
+import cardImg09 from "../../image/card/card09.png"
+
+export const cardImages = [{ src: cardImg01 }, { src: cardImg02 }, { src: cardImg03 }, { src: cardImg04 }, { src: cardImg05 }, { src: cardImg06 }, { src: cardImg07 }, { src: cardImg08 }, { src: cardImg09 }]
