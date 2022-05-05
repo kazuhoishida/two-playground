e33cdc7e68804ebd0bb56d4f4318791a0c38d85e
Kazuho Ishida <k10700110@gmail.com>
2022-05-06 03:20:44 +0900

    fix flip func

--- src/components/card/CardMain.jsx
+++ src/components/card/CardMain.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react"
 import SingleCard from "./SingleCard"
-import { moveContext, flipContext } from "./CardContext"
 import { cardImages } from "./cardImages"
 
 export default function CardMain() {
@@ -8,12 +7,11 @@ export default function CardMain() {
   const [rounds, setRounds] = useState(0)
   const [clickable, setClickable] = useState(true)
   const [isFirstMove, setIsFirstMove] = useState(true)
-  const value = { isFirstMove, setIsFirstMove }
   const [flipCardId, setFlipCardId] = useState({
     first: null,
     second: null,
   })
-  const flipValue = { flipCardId, setFlipCardId }
+  const [message, setMessage] = useState()
 
   const shuffleCards = () => {
     const shuffledDeck = [...cardImages, ...cardImages].sort(() => Math.random(0, 1) - 0.5).map((card) => ({ ...card, id: Math.random(0, 1), matched: false }))
@@ -21,31 +19,25 @@ export default function CardMain() {
     setRounds(0)
   }
 
-  //initialize card deck
-  useEffect(() => {
-    shuffleCards()
-  }, [])
-
   useEffect(() => {
     if (flipCardId.first === null || flipCardId.second === null) return
     setClickable(false)
 
     if (flipCardId.first.src === flipCardId.second.src) {
-      for (let i = 0; i < cards.length; i++) {
-        if (cards[i].src === flipCardId.first.src) cards[i].matched = true
-      }
-
-      for (let i = 0; i < cards.length; i++) {
-        if ((cards[i].matched = true)) return
-        console.log("finish")
-      }
+      setCards((prevDeck) => {
+        return prevDeck.map((card) => {
+          if (card.src === flipCardId.first.src) {
+            return { ...card, matched: true }
+          } else {
+            return card
+          }
+        })
+      })
 
       setFlipCardId({ first: null, second: null })
       setClickable(true)
-      setCards(cards)
     } else {
       setRounds(rounds + 1)
-
       setTimeout(() => {
         setFlipCardId({ first: null, second: null })
         setClickable(true)
@@ -53,24 +45,47 @@ export default function CardMain() {
     }
   }, [isFirstMove])
 
+  const flipCard = (card) => {
+    isFirstMove ? setFlipCardId({ ...flipCardId, first: card }) : setFlipCardId({ ...flipCardId, second: card })
+    setIsFirstMove(!isFirstMove)
+  }
+
+  //check if game is finished
+  useEffect(() => {
+    if (cards.length === 0) return
+    for (let i = 0; i < cards.length; i++) {
+      if (cards[i].matched === false) return
+    }
+
+    if (rounds < cards.length) {
+      setMessage("Congratulations!")
+    } else {
+      setMessage("not bad, actually")
+    }
+  }, [cards])
+
+  //initialize card deck
+  useEffect(() => {
+    shuffleCards()
+  }, [])
+
   return (
-    <moveContext.Provider value={value}>
-      <flipContext.Provider value={flipValue}>
-        <div className="pt-10 text-white">
-          <h1 className="text-center mb-10">TYPEFINDER</h1>
-          <div className="grid grid-cols-6 gap-4 w-4/5 mx-auto">
-            {cards.map((card, index) => (
-              <SingleCard card={card} turned={flipCardId.first == card || flipCardId.second == card || card.matched} clickable={clickable} key={`${card.id}-${index}`} />
-            ))}
-          </div>
-          <div className="flex justify-between absolute bottom-10 left-[10%] w-4/5">
-            <p className="">
-              <span className="mr-2">{rounds}</span>rounds
-            </p>
-            <button onClick={shuffleCards}>Shuffle</button>
-          </div>
+    <>
+      <div className="pt-10 text-white">
+        <h1 className="text-center text-16 mb-10">TYPEFINDER</h1>
+        <div className="grid grid-cols-6 gap-4 w-3/5 mx-auto">
+          {cards.map((card, index) => (
+            <SingleCard card={card} turned={flipCardId.first == card || flipCardId.second == card || card.matched} clickable={clickable} flipCard={flipCard} key={`${card.id}-${index}`} />
+          ))}
         </div>
-      </flipContext.Provider>
-    </moveContext.Provider>
+        <p className="text-center mt-10">{message}</p>
+        <div className="flex justify-between absolute bottom-10 left-[10%] w-4/5">
+          <p className="">
+            <span className="mr-2">{rounds}</span>rounds
+          </p>
+          <button onClick={shuffleCards}>Shuffle</button>
+        </div>
+      </div>
+    </>
   )
 }
--- src/components/card/SingleCard.jsx
+++ src/components/card/SingleCard.jsx
@@ -1,20 +1,12 @@
-import { useContext } from "react"
-import { moveContext, flipContext } from "./CardContext"
-
-export default function SingleCard({ card, turned, clickable }) {
-  const { isFirstMove, setIsFirstMove } = useContext(moveContext)
-  const { flipCardId, setFlipCardId } = useContext(flipContext)
-
+export default function SingleCard({ card, turned, clickable, flipCard }) {
   const handleFlipCard = () => {
     if (turned || !clickable) return
-
-    isFirstMove ? setFlipCardId({ ...flipCardId, first: card }) : setFlipCardId({ ...flipCardId, second: card })
-    setIsFirstMove(!isFirstMove)
+    flipCard(card)
   }
   return (
     <div className={turned ? "card-style flipped-style" : "card-style normal-style"}>
-      <div style={{ backgroundImage: `url(${card.src})` }} className="frontface w-full h-full absolute top-0 left-0 z-0" onClick={handleFlipCard} />
-      <div style={{ backgroundImage: "url(../../image/card/back.png)" }} className="backface w-full h-full absolute top-0 left-0 z-10" onClick={handleFlipCard} />
+      <div style={{ backgroundImage: `url(${card.src})` }} className="frontface card-image" />
+      <div style={{ backgroundImage: "url(../../image/card/back.png)" }} className="backface card-image" onClick={handleFlipCard} />
     </div>
   )
 }
