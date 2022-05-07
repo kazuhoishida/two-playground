2b34b6b2f4e848b2d9ad62bd9fdbf14b7214789b
Kazuho Ishida <k10700110@gmail.com>
2022-05-08 02:31:29 +0900

    fix typo

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
         neue: resolve(__dirname, "src/neue/index.html"),
         typefinder: resolve(__dirname, "src/typefinder/index.html"),
         readymag: resolve(__dirname, "src/readymag/index.html"),
-        vitamontreal: resolve(__dirname, "src/vitamontreal/index.html"),
+        vivamontreal: resolve(__dirname, "src/vivamontreal/index.html"),
       },
     },
   },
