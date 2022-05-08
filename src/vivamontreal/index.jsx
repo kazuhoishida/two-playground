5a4a955b4e3ce0c5cb53998ed53ab9a6842e153e
Kazuho Ishida <k10700110@gmail.com>
2022-05-08 20:36:56 +0900

    add footer on viva page

--- src/vivamontreal/index.jsx
+++ src/vivamontreal/index.jsx
@@ -7,11 +7,25 @@ import VivaDefyGravity from "../components/vivamontreal/VivaDefyGravity"
 function VivamontrealApp() {
   return (
     <div className="relative">
-      <div className="fixed top-0 left-0 w-full text-36 font-bold flex justify-between px-20 pt-6 z-50">
+      <div className="fixed top-0 left-0 w-full text-36 font-bold flex justify-between px-10 pt-6 z-50">
         <p>VIVA!</p>
         <p>Defying Gravity</p>
       </div>
       <VivaDefyGravity />
+      <div className="fixed bottom-4 left-0 flex justify-between items-end px-10 w-full">
+        <p>
+          Ateliers Jean Brillant,
+          <br />
+          661 Rose de Lima, Tiohtiá:ke/Montréal
+          <br />
+          (Métro Lionel-Groulx)
+        </p>
+        <ul className="flex justify-end">
+          <li className="mr-4">Facebook</li>
+          <li className="mr-4">Instagram</li>
+          <li>Twitter</li>
+        </ul>
+      </div>
     </div>
   )
 }
