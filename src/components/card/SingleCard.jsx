b6986e1bca7bf3b052242e919a29c14db4533778
Kazuho Ishida <k10700110@gmail.com>
2022-05-11 00:27:25 +0900

    add japanese font deck

--- src/components/card/SingleCard.jsx
+++ src/components/card/SingleCard.jsx
@@ -1,8 +1,9 @@
 import { useAtom } from "jotai"
 import { isFirstMoveAtom, flipCardIdAtom } from "./CardContext"
 import backImg from "../../image/card/back.png"
+import backImgJA from "../../image/card/back-JA.png"
 
-export default function SingleCard({ card, turned, clickable, deg }) {
+export default function SingleCard({ card, isEnglish, turned, clickable, deg }) {
   const [isFirstMove, setIsFirstMove] = useAtom(isFirstMoveAtom)
   const [flipCardId, setFlipCardId] = useAtom(flipCardIdAtom)
 
@@ -17,7 +18,7 @@ export default function SingleCard({ card, turned, clickable, deg }) {
     <div className="transition-all" style={{ transform: `rotate(${deg}deg)` }}>
       <div className={turned ? "card-style flipped-style" : "card-style normal-style"}>
         <img src={card.src} alt="card front" className="frontface card-image" />
-        <img src={backImg} alt="card back" className="backface card-image" onClick={handleFlipCard} />
+        <img src={isEnglish ? backImg : backImgJA} alt="card back" className="backface card-image" onClick={handleFlipCard} />
       </div>
     </div>
   )
